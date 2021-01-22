f3f7c1e324e493386be75eae4af050a01a5a8e0f
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2021-01-21 23:50:23 -0500

    added day 2

--- 30_days_of_code.rb
+++ 30_days_of_code.rb
@@ -21,3 +21,23 @@ puts d + gets.to_f
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
 puts s + gets
+
+#Day 2
+
+require "json"
+require "stringio"
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent)
+end
+
+meal_cost = gets.to_f
+
+tip_percent = gets.to_i
+
+tax_percent = gets.to_i
+
+solve meal_cost, tip_percent, tax_percent
+result = meal_cost * (1 + tip_percent.to_f / 100 + tax_percent.to_f / 100)
+
+puts result.round()
